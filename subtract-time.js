e58b39fadee146036521da82290e7a853208d8f9
Andreas Loeffler <aloeff@de.ibm.com>
2017-04-06 17:47:11 +0200

    test script

--- subtract-time.js
+++ subtract-time.js
@@ -12,7 +12,22 @@ var endtime   = process.argv[3];
 
 console.log("start: " + starttime);
 console.log("end  : " + endtime);
+var d =  moment.utc(moment(endtime).diff(moment(starttime))).format("HH:mm");
+
+console.log(d);
 
 
-console.log(moment(endtime).subtract(starttime).format('hh:mm'));
 
+//var ms = moment(endtime, "YYYY-MM-DD HH:mm").diff(moment(starttime, "YYYY-MM-DD HH:mm"));
+//var d = moment.duration(ms);
+//var s = Math.floor(d.asHours()) + moment.utc(ms).format(":mm");
+
+
+// console.log(moment('Fri, 24 Mar 2017 10:36:12 +0100').format('hh:mm'));
+// console.log(moment("2017-03-24 10:01:40").format('hh:mm'));//.tz("Europe/Berlin").format('Z');
+// console.log(moment('2017-03-24 10:48').format('hh:mm'));
+
+
+// example:
+// ./subtract-time.js "2017-04-05 08:05" "2017-04-05 17:54"
+// 
