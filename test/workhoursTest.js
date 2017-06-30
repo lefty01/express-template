6f4930455317072d6b79c9f4deb273d881a6c627
Andreas Loeffler <aloeff@de.ibm.com>
2017-06-30 16:32:18 +0200

    test the testing

---
+++ test/workhoursTest.js
@@ -0,0 +1,10 @@
+
+suite('crud', function () {
+  test('returns true for numbers divisible by 3', function () {
+    // ...
+  });
+  test('returns false for numbers not divisible by 3', function () {
+    // ...
+  });
+});
+
---
+++ test/worktimes.js
@@ -0,0 +1,10 @@
+
+//test('Numbers divisible by 3 return Fizz');
+
+test('Foobar test for myworktimes. new entry should be START or END', function () {
+  this.timeout(5 * 1000);
+  // ...
+
+});
+  
+  
